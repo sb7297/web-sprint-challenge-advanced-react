96321b28f31c957754207c211184e9ca1291c902
sb7297 <sbaireddy7297@gmail.com>
2021-08-06 17:19:13 -0400

    PlantList: get plants from api

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,6 +3,12 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    };
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -11,8 +17,13 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        
-      });
+        this.setState({
+          plants: res.data
+        });
+      })
+      .catch(err => {
+        console.log("oh no! couldn't get plants, error: ", err);
+      })
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
